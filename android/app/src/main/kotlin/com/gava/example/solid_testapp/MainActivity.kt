d3ba33174de8ab6f0051900388ae27d544af1d13
E.gajvor <777gava777@gmail.com>
2019-07-07 12:10:15 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/gava/example/solid_testapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.gava.example.solid_testapp
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
