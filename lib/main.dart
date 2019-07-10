62490a74a9fdd1f9258ce8c23d6313096b98d8cc
E.gajvor <777gava777@gmail.com>
2019-07-11 01:24:27 +0300

    working app

--- lib/main.dart
+++ lib/main.dart
@@ -2,22 +2,10 @@ import 'package:flutter/material.dart';
 import 'dart:math';
 
 void main() {
-  runApp(MaterialApp(home: HomePage()));
-}
-
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Solid Software Test Task',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: MyHomePage(),
-
-    );
-  }
+  runApp(MaterialApp(
+    home: MyHomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
 class MyHomePage extends StatefulWidget {
@@ -26,12 +14,29 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final Random _random = Random();
+  _showMenu() async {
+    await showMenu(
+      context: context,
+      position: RelativeRect.fromLTRB(100, 400, 100, 400),
+      items: [
+        PopupMenuItem(
+          child: Text("send feedback"),
+        ),
+        PopupMenuItem(
+          child: Text("777gava777@gmail.com"),
+        ),
+      ],
+      elevation: 10.0,
+    );
+  }
 
+  final Random _random = Random();
+  bool changeText = false;
   Color _color = Color(0xFFFFFFFF);
 
   void changeColor() {
     setState(() {
+      changeText = !changeText;
       _color = Color.fromARGB(
         _random.nextInt(256),
         _random.nextInt(256),
@@ -44,15 +49,36 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: InkWell(
-          onTap: changeColor,
-          child: Container(
-            color: _color,
-
-
-
-          ),
-        )
-    );
+        body: GestureDetector(
+      onLongPress: _showMenu,
+      onTap: changeColor,
+      child: Container(
+        color: _color,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Center(
+              child: AnimatedDefaultTextStyle(
+                  style: changeText
+                      ? TextStyle(
+                          fontSize: 65,
+                          color: Colors.white,
+                          fontWeight: FontWeight.w100,
+                        )
+                      : TextStyle(
+                          fontSize: 25,
+                          color: Colors.black,
+                          fontWeight: FontWeight.normal,
+                        ),
+                  duration: const Duration(microseconds: 150),
+                  curve: Curves.bounceInOut,
+                  child: Text("Hey there!")),
+            )
+          ],
+        ),
+      ),
+    ));
   }
 }
